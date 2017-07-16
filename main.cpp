454687da08b7e1a59521f6cf74d37c63f804588d
cotyb <xieleicotyb@gmail.com>
2017-07-16 22:31:26 +0800

    addtwonumbers

--- main.cpp
+++ main.cpp
@@ -27,23 +27,36 @@ public:
 
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *res = l1, *mid = l1;
+        ListNode *res = l1, *cur_node = l1;
         int c = 0;
-        while(l1 && l2){
-            int tmp = l1->val + l2->val + c;
-            l1->val = tmp % 10;
-            c = tmp / 10;
-            mid = l1;
-            l1 = l1->next;
-            l2 = l2->next;
+        ListNode *tmp;
+        int tmp1, tmp2;
+        while (l1 != NULL || l2 !=NULL) {
+            if(l1 == NULL){
+                cur_node->next = l2;
+                l2 = l1;
+                l1 = cur_node->next;
+            }
+            if(l2 == NULL){
+                tmp1 = l1->val + c;
+                l1->val = tmp1 % 10;
+                c = tmp1 / 10;
+                cur_node = l1;
+                l1 = l1->next;
+            } else{
+                tmp2 = l1->val + l2->val + c;
+                l1->val = tmp2 % 10;
+                c = tmp2 / 10;
+                cur_node = l1;
+                l1 = l1->next;
+                l2 = l2->next;
+            }
         }
-        if(l2){
-            l2->val += c;
-            mid->next = l2;
-        } else{
-            l1->val += c;
+        if(c==1){
+            cur_node->next = new ListNode(1);
         }
-        return res;
+        l1 = res;
+        return l1;
     }
 };
 
@@ -52,6 +65,7 @@ public:
 
 int main(){
     //int n[] = {};
+    /*
     vector<int> nums = {2, 7, 11, 15};
     int target = 9;
     Solution solution = Solution();
@@ -59,5 +73,18 @@ int main(){
     for(int i=0; i<result.size(); i++){
         cout << result[i] << " ";
     }
+    */
+    ListNode *listNode1 = new ListNode(5);
+    ListNode *listNode2 = new ListNode(9);
+    ListNode *listNode3 = new ListNode(5);
+    listNode1->next = listNode2;
+
+    Solution solution = Solution();
+    ListNode *res = solution.addTwoNumbers(listNode3, listNode1);
+    while (res){
+        cout << res->val;
+        res=res->next;
+    }
+
     return 0;
 }
